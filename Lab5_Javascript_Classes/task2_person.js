909c590ae5e688b909a3d21a309fa33829c60804
Anette Kniberg <anette.kniberg@nordron.com>
2020-11-26 14:56:08 +0100

    Lab5 task2 done

--- Lab5_Javascript_Classes/task2_person.js
+++ Lab5_Javascript_Classes/task2_person.js
@@ -3,22 +3,11 @@ class Person {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
-        this.email = email;
-        
-        
+        this.email = email;  
     }
-    
     toString() {
-        return this.firstName + ' '+ this.lastName
+        return this.firstName + ' '+ this.lastName + '(age: ' + this.age + ', email: '+ this.email +')';
     }
 }
 let person = new Person('Maria', 'Petterson', 22, 'mp@gmail.com');
-
-
-console.log(person);    // gives classname and constructor
-console.log(''+ person); //calls  my toString() method
-
-//testing console prints from another object
-let a = new Date()
-console.log(a)
-console.log(a.toString())
+console.log(''+ person); //gives my toString()
