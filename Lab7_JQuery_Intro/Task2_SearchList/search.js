5fcea8e5845f7319ebe3d457ab097e7020445f1b
Anette Kniberg <anette.kniberg@nordron.com>
2020-12-02 00:46:37 +0100

    WIP Lab7 task2 searchList: Weird issue

--- Lab7_JQuery_Intro/Task2_SearchList/search.js
+++ Lab7_JQuery_Intro/Task2_SearchList/search.js
@@ -2,6 +2,12 @@ function search() {
  var value = $("#searchText").val();
  console.log(value);
 
- $("li:contains('Ma')").css("font-weight","bold");
- //todo. check how to inser variable istead of 'Ma'
+//  $("li:contains(`${value}´)").css("font-weight","bold");
+$("li:contains('Ma')").css("font-weight","bold");
+//above is very strange...
+ var result = $("li:contains(`${value}´)").data.length;
+ 
+ $("#result").text(result+" matches found");
+ 
+ 
 }
