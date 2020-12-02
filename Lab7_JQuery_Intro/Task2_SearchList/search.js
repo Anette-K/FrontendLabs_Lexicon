5a79f0f1f7a93e618a0297754b32743d27946aab
Anette Kniberg <anette.kniberg@nordron.com>
2020-12-02 07:55:16 +0100

    WIP Lab7 task2 SearchList: Done

--- Lab7_JQuery_Intro/Task2_SearchList/search.js
+++ Lab7_JQuery_Intro/Task2_SearchList/search.js
@@ -1,13 +1,9 @@
 function search() {
- var value = $("#searchText").val();
- console.log(value);
 
-//  $("li:contains(`${value}´)").css("font-weight","bold");
-$("li:contains('Ma')").css("font-weight","bold");
-//above is very strange...
- var result = $("li:contains(`${value}´)").data.length;
- 
- $("#result").text(result+" matches found");
- 
+    var value = $("#searchText").val();
+    $("li:contains('" + value + "')").css("font-weight","bold");
+
+    var result = $("li:contains(`${value}´)").data.length;
+    $("#result").text(result + " matches found");
  
 }
