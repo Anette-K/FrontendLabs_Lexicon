21c1355f36839d2cf9245bb199cf0f5a4f8256ab
Anette Kniberg <anette.kniberg@nordron.com>
2020-12-03 17:03:52 +0100

    WIP

--- Lab7_JQuery_Intro/Task3_TableCountries/initialize-table.js
+++ Lab7_JQuery_Intro/Task3_TableCountries/initialize-table.js
@@ -1,15 +1,39 @@
 function initializeTable() {
-    
-    $("#createLink").click(function(){
-        var country = $("#newCountryText").val();
-        var city = $("#newCapitalText").val();
-        console.log(city);
+
+    $("#createLink").click(function() {
+        let country = $("#newCountryText").val();
+        let city = $("#newCapitalText").val();
         
-        var markup = "<tr><td>" + country + "</td><td>" + city + "</td><td><a href='#' id='delete'>[Delete]</a></td></tr>" ;
+        
+        //if table only only has two rows= only have delete link
+        let markup = "<tr><td>" + country + "</td><td>" + city + "</td><td><a href='#' id='delete'>[Delete]</a></td></tr>" ;
         $("#countriesTable").append(markup);
         
-       
+        //if table has >2 rows: new insert also gets a link for up. The one before recieves a link for down.
+        if ( $("#countriesTable tr").length > 3) {
+            let upLink = "<a href='#' id='up'>[Up]</a>";
+            $("a:last").parent().append(upLink);
+
+        }
+        
+        // clear input
+        $("#newCountryText, #newCapitalText" ).val('');
+        
     });
+    
+    $("#countriesTable").on("click", "#delete",function(){ // .on(click,..) works on dynamically added elements
+        console.log("test");
+        
+
+    });
+
+
+    function updateLink() {
+        //cases: 
+        // if element has a row above: add a link for up, if there isn't one (was first)
+        // if element has a row below: add a link for down if there isn't one. (was last)
+        // 
+    }
     
 }
 
