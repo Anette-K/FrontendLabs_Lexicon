1e52af00689d157c07f669b2cd76eadd16b71302
Anette Kniberg <anette.kniberg@nordron.com>
2020-12-04 00:35:38 +0100

    WIP still task3:  non-triggering if clauses.

--- Lab7_JQuery_Intro/Task3_TableCountries/initialize-table.js
+++ Lab7_JQuery_Intro/Task3_TableCountries/initialize-table.js
@@ -1,38 +1,65 @@
 function initializeTable() {
 
-    $("#createLink").click(function() {
+    $("#countriesTable").on("click","#createLink",function() {
+
         let country = $("#newCountryText").val();
         let city = $("#newCapitalText").val();
+        let rowHtml = "<tr><td>" + country + "</td><td>" + city + "</td><td></td></tr>" ;
+        $("#countriesTable").append(rowHtml);
         
+        let delLink = "<a href='#' id='delete'>[Delete]</a>";
+        let upLink = "<a href='#' id='up'>[Up]</a>";
+        let downLink = "<a href='#' id='down'>[Down]</a>";
+        $("td:last").append(delLink, upLink, downLink);
         
-        //if table only only has two rows= only have delete link
-        let markup = "<tr><td>" + country + "</td><td>" + city + "</td><td><a href='#' id='delete'>[Delete]</a></td></tr>" ;
-        $("#countriesTable").append(markup);
-        
-        //if table has >2 rows: new insert also gets a link for up. The one before recieves a link for down.
-        if ( $("#countriesTable tr").length > 3) {
-            let upLink = "<a href='#' id='up'>[Up]</a>";
-            $("a:last").parent().append(upLink);
-
+        // initial settings of the links (which to hide)
+        if ( $("#countriesTable tr").length == 3) {
+            $("#up").toggle();
+            $("#down").toggle();
+                        
         }
+        else $("td:last #down, td:last-child #down:eq(-2)").toggle();
         
-        // clear input
+        // clear input field
         $("#newCountryText, #newCapitalText" ).val('');
         
     });
     
     $("#countriesTable").on("click", "#delete",function(){ // .on(click,..) works on dynamically added elements
         console.log("test");
+        let anchor = $(this);
         
-
+        //trying to test here.not working. 
+        if(anchor.parent().is("td:last")) {console.log("is last")}
+        if(anchor.is(":first")) {console.log("is first")}
+        //
+       //not working
+       // updateLink(anchor);
+        
+        $(this).parents("tr").remove();
     });
 
-
-    function updateLink() {
-        //cases: 
-        // if element has a row above: add a link for up, if there isn't one (was first)
-        // if element has a row below: add a link for down if there isn't one. (was last)
-        // 
+    //not working.wip
+    function updateLink(anchor) {
+        
+        let id = anchor.attr("id"); //
+        console.log(anchor.html()); //
+        //problem: if clause not getting triggered. find correct syntax.
+        if(anchor.parent().is("td:last")) {console.log("is last")}
+        if(anchor.is(":first")) {console.log("is first")}
+        else console.log("middle");
+        
+        
+        //pseudo: 
+        // if clicked anchor is first in table: 
+        //     hide uplink 
+        //     show downlink
+        
+        // if row is last : 
+        //     hide downlink
+        //     show uplink
+        // else: show all 3 links.
+        
     }
     
 }
